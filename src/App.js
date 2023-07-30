6898e8d18716e8b166887e3b3ba23b8543d20391
zuhaib <zuh1635@gmail.com>
2023-07-30 11:58:13 -0700

    Navbar and Previous and Next button functionality is added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+
+export default class App extends Component {
+  
+  render() {
+    //render(): Ccompile JSX into HTML then render the given HTML
+    return (
+      <div>
+       <Navbar/>
+       <News/>
+      </div>
+    )
+  }
+}
+
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+
+    render() {
+        let { title, description, imageUrl, newsUrl } = this.props
+        return (
+            <div className='my-3'>
+                <div className="card" style={{width: "18rem"}}>
+                    {/* if image URL is null run the default URL */}
+                    <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2023/07/29/600x338/US_China_Taiwan_Weapons_1690621942541_1690621942849.jpg":imageUrl} className="card-img-top" alt="Unalbe to load" />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
